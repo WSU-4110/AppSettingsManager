a0fad5bc07378040f3e8777ed6349180a96e4c79
flennaja <hg2956@wayne.edu>
2023-02-06 14:47:02 -0500

    initial migration

---
+++ AppSettingsManagerAPI/AppSettingsManagerApi.Infrastructure/Migrations/20230206194322_InitialCreate.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AppSettingsManagerApi.Infrastructure.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "BaseUsers",
+                columns: table => new
+                {
+                    UserId = table.Column<string>(type: "varchar(36)", maxLength: 36, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    UserName = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Password = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BaseUsers", x => x.UserId);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BaseUsers");
+        }
+    }
+}
--- AppSettingsManagerAPI/AppSettingsManagerApi.Infrastructure/MySql/SettingsContext.cs
+++ AppSettingsManagerAPI/AppSettingsManagerApi.Infrastructure/MySql/SettingsContext.cs
@@ -1,5 +1,4 @@
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
 
 namespace AppSettingsManagerApi.Infrastructure.MySql;
 
