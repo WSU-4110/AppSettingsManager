6c2aec693c3d1a2dd6199b4225a240c302c8a6c2
flennaja <hg2956@wayne.edu>
2023-02-15 08:25:45 -0500

    dunno how the program.cs got deleted

---
+++ AppSettingsManagerAPI/AppSettingsManagerApi/Program.cs
@@ -0,0 +1,45 @@
+using AppSettingsManagerApi.Infrastructure.MySql;
+using AppSettingsManagerApi.Infrastructure.MySql.Converters;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc(
+        "v1",
+        new Microsoft.OpenApi.Models.OpenApiInfo
+        {
+            Title = "App Settings Manager v1",
+            Version = "v1"
+        }
+    );
+});
+
+builder.Services.AddConverters();
+
+builder.Services.AddMySqlSettingsStorage(
+    builder.Configuration.GetConnectionString("DefaultConnection")
+);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
