d79b8fd126e50c207bd1aaf524dbf463389b6594
Mustafa Marini <Mustafamarini25@gmail.com>
2023-02-20 00:18:49 -0500

    Added files

--- AppSettingsManagerBff/AppSettingsManagerBff.Domain/ApiRepositories/IUserRepository.cs
+++ AppSettingsManagerBff/AppSettingsManagerBff.Domain/ApiRepositories/IUserRepository.cs
@@ -5,5 +5,5 @@ namespace AppSettingsManagerBff.Domain.ApiRepositories;
 public interface IUserRepository
 {
     Task<ApiBaseUser> GetUser(string userId);
-    Task<ApiBaseUser> CreateUser(string userId, string password);
+    Task<ApiBaseUser> CreateUser(string userId, string password); 
 }
