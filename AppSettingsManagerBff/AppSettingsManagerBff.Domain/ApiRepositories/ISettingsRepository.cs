918d1578a83d8e6afd51dcf58d5a198154286ea9
Jaberul Haque <56807723+arushwolf@users.noreply.github.com>
2023-02-21 08:34:52 -0500

    BFF CRUD Settings (#40)
    * completed BFF CRUD commands for Settings
    
    * committing fix for api controller routing
    
    as per Jack's suggestion
    
    Co-authored-by: Jack Flenna <jackflenna@gmail.com>
    
    * committing fix for api udateSetting routing
    
    got rid of unnecessary routing
    
    Co-authored-by: Jack Flenna <jackflenna@gmail.com>
    
    * committing fix for api deleteSetting routing
    
    as per Jack's suggestion, adding the proper routing
    
    Co-authored-by: Jack Flenna <jackflenna@gmail.com>
    
    * committing fix for api deleteSetting referencing
    
    Jack noticed that the reference did not match with the header in the SettingsController file within ASMApi/Contollers folder
    
    Co-authored-by: Jack Flenna <jackflenna@gmail.com>
    
    * fixed some dependency issues
    
    * fixing line 104
    
    Jack caught a mistake in 104
    
    Co-authored-by: Jack Flenna <jackflenna@gmail.com>
    
    * fixing line 38
    
    Jack caught a mistake on line 38
    
    Co-authored-by: Jack Flenna <jackflenna@gmail.com>
    
    * fixed
    
    ---------
    
    Co-authored-by: Jack Flenna <jackflenna@gmail.com>
    Co-authored-by: flennaja <hg2956@wayne.edu>

--- AppSettingsManagerBff/AppSettingsManagerBff.Domain/ApiRepositories/ISettingsRepository.cs
+++ AppSettingsManagerBff/AppSettingsManagerBff.Domain/ApiRepositories/ISettingsRepository.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using AppSettingsManagerBff.Model.Api;
 
 namespace AppSettingsManagerBff.Domain.ApiRepositories;
@@ -5,4 +6,7 @@ namespace AppSettingsManagerBff.Domain.ApiRepositories;
 public interface ISettingsRepository
 {
     Task<ApiSetting> CreateSetting(CreateSettingRequest request);
+    Task<ApiSetting> GetSetting(string settingId, int version);
+    Task<ApiSetting> UpdateSetting(UpdateSettingRequest request);
+    Task<IEnumerable<ApiSetting>> DeleteSetting(string settingId);
 }
