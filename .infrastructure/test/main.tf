5087d27f26718c817c35aae737f56f547d369cbb
flennaja <jackflenna@wayne.edu>
2023-03-16 19:16:45 -0400

    fixing networking

--- .infrastructure/test/main.tf
+++ .infrastructure/test/main.tf
@@ -94,6 +94,17 @@ resource "aws_route_table_association" "table2" {
   route_table_id = aws_route_table.route_table.id
 }
 
+resource "aws_vpc_endpoint" "s3" {
+  vpc_id            = aws_vpc.vpc.id
+  service_name      = "com.amazonaws.us-east-2.s3"
+  vpc_endpoint_type = "Interface"
+
+  security_group_ids = [aws_security_group.ecs_sg.id]
+  subnet_ids         = [aws_subnet.subnet.id, aws_subnet.subnet2.id]
+
+  private_dns_enabled = true
+}
+
 resource "aws_vpc_endpoint" "ecr_dkr" {
   vpc_id            = aws_vpc.vpc.id
   service_name      = "com.amazonaws.us-east-2.ecr.dkr"
