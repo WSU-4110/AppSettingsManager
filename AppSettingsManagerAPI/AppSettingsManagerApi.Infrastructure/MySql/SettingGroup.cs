36606f8bf789e590004a380d20838fadcad8acba
Mustafa Marini <Mustafamarini25@gmail.com>
2023-03-20 04:33:28 -0400

    SettingGroup Changes

--- AppSettingsManagerAPI/AppSettingsManagerApi.Infrastructure/MySql/SettingGroup.cs
+++ AppSettingsManagerAPI/AppSettingsManagerApi.Infrastructure/MySql/SettingGroup.cs
@@ -4,11 +4,11 @@ namespace AppSettingsManagerApi.Infrastructure.MySql;
 
 public class SettingGroup
 {
-    public string SettingGroupId { get; set; }
+    public string Id { get; set; }
     public string CreatedBy { get; set; }
 
     [Timestamp]
     public byte[] LastUpdatedAt { get; set; }
-    public List<Setting> Settings { get; set; }
-    public List<Permission> Permissions { get; set; }
+    public List<Setting> Settings { get; set; } = new List<Setting>();
+    public List<Permission> Permissions { get; set; } = new List<Permission>();
 }
