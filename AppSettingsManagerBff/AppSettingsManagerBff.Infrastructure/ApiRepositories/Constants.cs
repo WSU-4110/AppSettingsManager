df1668e9cd8ea8a2e234d746c6bde5ee784caf9b
arushwolf <h.jaberul2020@gmail.com>
2023-04-04 16:58:02 -0400

    revamped infrastructure from ground-up

---
+++ AppSettingsManagerBff/AppSettingsManagerBff.Infrastructure/ApiRepositories/Constants.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace AppSettingsManagerBff.Infrastructure.ApiRepositories;
+
+public static class Constants
+{
+    private const string RequestMediaType = "application/json";
+
+    private static readonly JsonSerializerOptions JsonSerializerOptions = new JsonSerializerOptions
+    {
+        PropertyNameCaseInsensitive = true
+    };
+
+    public static StringContent GetStringContent(object request) =>
+        new StringContent(JsonSerializer.Serialize(request), Encoding.UTF8, RequestMediaType);
+
+    public static async Task<T> DeserializeResponse<T>(HttpResponseMessage response) =>
+        JsonSerializer.Deserialize<T>(
+            await response.Content.ReadAsStringAsync(),
+            JsonSerializerOptions
+        ) ?? throw new HttpRequestException("Method returned null object");
+
+    public static string GetRequestUri(string userId, string password, string? settingGroupId = null) =>
+        string.IsNullOrEmpty(settingGroupId)
+            ? $"userId/{userId}/password/{password}"
+            : $"userId/{userId}/password/{password}/settingGroupId/{settingGroupId}";
+}
