126853c04537789285c6b5a66235db0dcd6ac69a
arushwolf <h.jaberul2020@gmail.com>
2023-04-06 11:52:58 -0400

    C ops of CRUD for requests

---
+++ AppSettingsManagerBff/AppSettingsManagerBff.Model/Requests/CreateSettingRequest.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Nodes;
+
+namespace AppSettingsManagerBff.Model.Requests;
+
+public class CreateSettingRequest
+{
+    public string SettingGroupId { get; set; }
+    public JsonNode Input { get; set; }
+    public string UserId { get; set; }
+    public string Password { get; set; }
+}
