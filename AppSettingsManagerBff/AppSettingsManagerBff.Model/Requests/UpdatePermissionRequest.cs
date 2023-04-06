e66605efe54a35a4617d96cd6170897ea5564167
arushwolf <h.jaberul2020@gmail.com>
2023-04-06 11:54:54 -0400

    U ops of CRUD for some requests

---
+++ AppSettingsManagerBff/AppSettingsManagerBff.Model/Requests/UpdatePermissionRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace AppSettingsManagerBff.Model.Requests;
+
+public class UpdatePermissionRequest
+{
+    public string UserId { get; set; }
+    public string SettingGroupId { get; set; }
+    public PermissionLevel NewPermissionLevel { get; set; }
+}
---
+++ AppSettingsManagerBff/AppSettingsManagerBff.Model/Requests/UpdateTargetSettingRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace AppSettingsManagerBff.Model.Requests;
+
+public class UpdateTargetSettingRequest
+{
+    public string SettingGroupId { get; set; }
+    public int NewCurrentVersion { get; set; }
+    public string UserId { get; set; }
+    public string Password { get; set; }
+}
