a8154a50dc97165405ddf39af6ecfdaab5956c75
Nahiyan Chowdhury <nahiyan898@gmail.com>
2023-04-15 16:36:55 -0400

    Added all unit tests for Permissions

--- AppSettingsManagerAPI/AppSettingsManagerApi.Infrastructure.Tests/MySqlPermissionRepositoryTests.cs
+++ AppSettingsManagerAPI/AppSettingsManagerApi.Infrastructure.Tests/MySqlPermissionRepositoryTests.cs
@@ -1,9 +1,13 @@
-using AppSettingsManagerApi.Domain.Conversion;
-using AppSettingsManagerApi.Model;
-using AppSettingsManagerApi.Infrastructure.MySql;
-using Permission = AppSettingsManagerApi.Infrastructure.MySql.Permission;
-using AppSettingsManagerApi.Tests;
 using AutoFixture;
+using AppSettingsManagerApi.Tests;
+using AppSettingsManagerApi.Domain.Conversion;
+using AppSettingsManagerApi.Model.Requests;
+using AppSettingsManagerApi.Infrastructure.MySql;
+using User = AppSettingsManagerApi.Infrastructure.MySql.User;
+using Permission = AppSettingsManagerApi.Infrastructure.MySql.Permission;
+using SettingGroup = AppSettingsManagerApi.Infrastructure.MySql.SettingGroup;
+using PermissionLevel = AppSettingsManagerApi.Model.PermissionLevel;
+
 namespace AppSettingsManagerApi.Infrastructure.Tests;
 
 public class MySqlPermissionRepositoryTests : IDisposable
@@ -29,6 +33,48 @@ public class MySqlPermissionRepositoryTests : IDisposable
     
     [Theory]
     [AutoTestData]
+    public async Task UpdatePermission_ReturnsPermission(UpdatePermissionRequest request)
+    {
+        var user = _fixture.Build<User>().Create();
+        user.Id = request.UserId;
+        _settingsContext.Users.Add(user);
+
+        var settingGroup = _fixture.Build<SettingGroup>().Create();
+        settingGroup.Id = request.SettingGroupId;
+        _settingsContext.SettingGroups.Add(settingGroup);
+
+        var permission = _fixture.Build<Permission>().Create();
+        permission.UserId = request.UserId;
+        permission.User = user;
+        permission.SettingGroupId = request.SettingGroupId;
+        permission.SettingGroup = settingGroup;
+
+        _settingsContext.Permissions.Add(permission);
+        await _settingsContext.SaveChangesAsync();
+
+        var expectedPermission = _fixture.Build<Model.Permission>().Create();
+
+        _permissionConverterMock
+            .Setup(
+                s =>
+                    s.Convert(
+                        It.Is<Permission>(
+                            p =>
+                                p.SettingGroupId == request.SettingGroupId
+                                && p.UserId == request.UserId
+                        )
+                    )
+            )
+            .Returns(expectedPermission);
+
+        var actualPermission = await _permissionRepository.UpdatePermission(request);
+
+        Assert.Equal(expectedPermission, actualPermission);
+    }
+
+
+    [Theory]
+    [AutoTestData]
     public async Task GetPermission_ThrowsInvalidOperationException(string userId, string settingGroupId)
     {
         await Assert.ThrowsAsync<InvalidOperationException>(
@@ -36,6 +82,184 @@ public class MySqlPermissionRepositoryTests : IDisposable
         );
 
     }
+
+    [Theory]
+    [AutoTestData]
+    public async Task GetPermission_ReturnsPermission(string userId, string settingGroupId)
+    {
+        var user = _fixture.Build<User>().Create();
+        user.Id = userId;
+        _settingsContext.Users.Add(user);
+
+        var settingGroup = _fixture.Build<SettingGroup>().Create();
+        settingGroup.Id = settingGroupId;
+        _settingsContext.SettingGroups.Add(settingGroup);
+
+        var permission = _fixture.Build<Permission>().Create();
+        permission.UserId = userId;
+        permission.User = user;
+        permission.SettingGroupId = settingGroupId;
+        permission.SettingGroup = settingGroup;
+
+        _settingsContext.Permissions.Add(permission);
+        await _settingsContext.SaveChangesAsync();
+
+        var expectedPermission = _fixture.Build<Model.Permission>().Create();
+
+        _permissionConverterMock
+            .Setup(
+                s =>
+                    s.Convert(
+                        It.Is<Permission>(
+                            p => p.SettingGroupId == settingGroupId && p.UserId == userId
+                        )
+                    )
+            )
+            .Returns(expectedPermission);
+
+        var actualPermission = await _permissionRepository.GetPermission(userId, settingGroupId);
+
+        Assert.Equal(expectedPermission, actualPermission);
+    }
+
+    [Theory]
+    [AutoTestData]
+    public async Task DeletePermission_ReturnsPermission(string userId, string settingGroupId)
+    {
+        var user = _fixture.Build<User>().Create();
+        user.Id = userId;
+        _settingsContext.Users.Add(user);
+
+        var settingGroup = _fixture.Build<SettingGroup>().Create();
+        settingGroup.Id = settingGroupId;
+        _settingsContext.SettingGroups.Add(settingGroup);
+
+        var permission = _fixture.Build<Permission>().Create();
+        permission.UserId = userId;
+        permission.User = user;
+        permission.SettingGroupId = settingGroupId;
+        permission.SettingGroup = settingGroup;
+
+        _settingsContext.Permissions.Add(permission);
+        await _settingsContext.SaveChangesAsync();
+
+        var expectedPermission = _fixture.Build<Model.Permission>().Create();
+
+        _permissionConverterMock
+            .Setup(
+                s =>
+                    s.Convert(
+                        It.Is<Permission>(
+                            p => p.SettingGroupId == settingGroupId && p.UserId == userId
+                        )
+                    )
+            )
+            .Returns(expectedPermission);
+
+        var actualPermission = await _permissionRepository.DeletePermission(userId, settingGroupId);
+
+        Assert.Equal(expectedPermission, actualPermission);
+    }
+
+    [Theory]
+    [AutoTestData]
+    public async Task CreatePermission_ReturnsPermission(CreatePermissionRequest request)
+    {
+        var user = _fixture.Build<User>().Create();
+        user.Id = request.UserId;
+        _settingsContext.Users.Add(user);
+
+        var settingGroup = _fixture.Build<SettingGroup>().Create();
+        settingGroup.Id = request.SettingGroupId;
+        _settingsContext.SettingGroups.Add(settingGroup);
+
+        var permission = _fixture.Build<Permission>().Create();
+        permission.UserId = request.UserId;
+        permission.User = user;
+        permission.SettingGroupId = request.SettingGroupId;
+        permission.SettingGroup = settingGroup;
+
+        var expectedPermission = _fixture.Build<Model.Permission>().Create();
+
+        _permissionConverterMock
+            .Setup(
+                s =>
+                    s.Convert(
+                        It.Is<Permission>(
+                            p =>
+                                p.SettingGroupId == request.SettingGroupId
+                                && p.UserId == request.UserId
+                        )
+                    )
+            )
+            .Returns(expectedPermission);
+
+        var actualPermission = await _permissionRepository.CreatePermission(request);
+
+        Assert.Equal(expectedPermission, actualPermission);
+    }
+
+    [Theory]
+    [AutoTestData]
+    public async Task CreatePermission_TrowsInvalidOperationException(CreatePermissionRequest request)
+    {
+        var user = _fixture.Build<User>().Create();
+        user.Id = request.UserId;
+        _settingsContext.Users.Add(user);
+
+        var settingGroup = _fixture.Build<SettingGroup>().Create();
+        settingGroup.Id = request.SettingGroupId;
+        _settingsContext.SettingGroups.Add(settingGroup);
+
+        var permission = _fixture.Build<Permission>().Create();
+        permission.UserId = request.UserId;
+        permission.User = user;
+        permission.SettingGroupId = request.SettingGroupId;
+        permission.SettingGroup = settingGroup;
+
+        _settingsContext.Permissions.Add(permission);
+        await _settingsContext.SaveChangesAsync();
+
+        await Assert.ThrowsAsync<InvalidOperationException>(
+            () => _permissionRepository.CreatePermission(request)
+        );
+    }
+
+    [Theory]
+    [AutoTestData]
+    public async Task GetPermissionFromContext_ReturnsPermission(string userId, string settingGroupId)
+    {
+        var user = _fixture.Build<User>().Create();
+        user.Id = userId;
+        _settingsContext.Users.Add(user);
+
+        var settingGroup = _fixture.Build<SettingGroup>().Create();
+        settingGroup.Id = settingGroupId;
+        _settingsContext.SettingGroups.Add(settingGroup);
+
+        var permission = _fixture.Build<Permission>().Create();
+        permission.UserId = userId;
+        permission.User = user;
+        permission.SettingGroupId = settingGroupId;
+        permission.SettingGroup = settingGroup;
+
+        _settingsContext.Permissions.Add(permission);
+        await _settingsContext.SaveChangesAsync();
+
+        var actualPermission = await _permissionRepository.GetPermissionFromContext(userId, settingGroupId);
+
+        Assert.Equal(permission, actualPermission);
+    }
+
+    [Theory]
+    [AutoTestData]
+    public async Task CheckPermission_ThrowsInvalidOperationException(string userId, string password, string settingGroupId, PermissionLevel permissionLevel)
+    {
+        await Assert.ThrowsAsync<InvalidOperationException>(
+            () => _permissionRepository.CheckPermission(userId, password, settingGroupId, permissionLevel)
+        );
+    }
+    
 
     public void Dispose()
     {
