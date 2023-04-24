ccf7367bcc76602f94f92ddecda65200de30df40
flennaja <jackflenna@wayne.edu>
2023-04-24 08:56:31 -0400

    bff tests done

--- AppSettingsManagerBff/AppSettingsManagerBff.Infrastructure.Tests/HttpSettingsRepositoryTests.cs
+++ AppSettingsManagerBff/AppSettingsManagerBff.Infrastructure.Tests/HttpSettingsRepositoryTests.cs
@@ -2,6 +2,8 @@ using AppSettingsManagerBff.Infrastructure.ApiRepositories;
 using System.Net;
 using System.Text.Json;
 using AppSettingsManagerBff.Model;
+using AppSettingsManagerBff.Model.Requests;
+using AutoFixture;
 using Moq.Protected;
 
 namespace AppSettingsManagerBff.Infrastructure.Tests;
@@ -14,16 +16,158 @@ public class HttpSettingsRepositoryTests
     {
         BaseAddress = "http://localhost:5000/"
     };
-
-    private HttpSettingsRepository GetHttpSettingsRepository()
+    
+    private readonly HttpClient _httpClient;
+    private readonly Fixture _fixture;
+    
+    public HttpSettingsRepositoryTests()
     {
-        var httpClient = new HttpClient(_mockHttpMessageHandler.Object)
-        {
-            BaseAddress = new Uri(_config.BaseAddress + "settings/")
-        };
-
-        return new HttpSettingsRepository(httpClient, _config);
+        _httpClient = new HttpClient(_mockHttpMessageHandler.Object);
+        
+        _httpClient.BaseAddress = new Uri(_config.BaseAddress + "settings/");
+        
+        _fixture = FixtureBuilder.BuildFixture();
     }
+
+    [Theory]
+    [AutoTestData]
+    public async Task GetSettingGroup_Succeeds(string userId, string password, string settingGroupId)
+    {
+        var settingGroup = _fixture.Create<SettingGroup>();
+        
+        SetupHttpMessageHandler(HttpStatusCode.OK, settingGroup);
+        
+        var httpSettingsRepository = GetHttpSettingsRepository();
+        
+        var result = await httpSettingsRepository.GetSettingGroup(userId, password, settingGroupId);
+        
+        Assert.Equal(settingGroup.Id, result.Id);
+    }
+    
+    [Theory]
+    [AutoTestData]
+    public async Task GetSettingGroupsForUser_Success(string userId, string password)
+    {
+        var settingGroups = _fixture.CreateMany<SettingGroup>();
+        
+        SetupHttpMessageHandler(HttpStatusCode.OK, settingGroups);
+        
+        var httpSettingsRepository = GetHttpSettingsRepository();
+        
+        var result = await httpSettingsRepository.GetSettingGroupsForUser(userId, password);
+        
+        Assert.True(result.Select(sg => sg.Id).OrderBy(sg => sg).SequenceEqual(settingGroups.Select(sg => sg.Id).OrderBy(sg => sg)));
+    }
+    
+    [Theory]
+    [AutoTestData]
+    public async Task CreateSettingGroup_Succeeds(string settingGroupId, string userId, string password, Dictionary<string, string> input)
+    {
+        var settingGroup = _fixture.Create<SettingGroup>();
+        
+        SetupHttpMessageHandler(HttpStatusCode.OK, settingGroup);
+        
+        var httpSettingsRepository = GetHttpSettingsRepository();
+
+        var createSettingRequest = new CreateSettingRequest
+        {
+            SettingGroupId = settingGroupId,
+            Input = JsonSerializer.SerializeToNode(input),
+            UserId = userId,
+            Password = password
+        };
+        
+        var result = await httpSettingsRepository.CreateSettingGroup(createSettingRequest);
+        
+        Assert.Equal(settingGroup.Id, result.Id);
+    }
+
+    [Theory]
+    [AutoTestData]
+    public async Task UpdateSetting_Succeeds(string settingGroupId, string userId, string password,
+        Dictionary<string, string> input)
+    {
+        var settingGroup = _fixture.Create<SettingGroup>();
+        
+        SetupHttpMessageHandler(HttpStatusCode.OK, settingGroup);
+        
+        var httpSettingsRepository = GetHttpSettingsRepository();
+        
+        var updateSettingRequest = new CreateSettingRequest
+        {
+            SettingGroupId = settingGroupId,
+            Input = JsonSerializer.SerializeToNode(input),
+            UserId = userId,
+            Password = password
+        };
+        
+        var result = await httpSettingsRepository.UpdateSetting(updateSettingRequest);
+        
+        Assert.Equal(settingGroup.Id, result.Id);
+    }
+
+    [Theory]
+    [AutoTestData]
+    public async Task ChangeTargetSettingVersion_Succeeds(UpdateTargetSettingRequest request)
+    {
+        var settingGroup = _fixture.Create<SettingGroup>();
+        
+        SetupHttpMessageHandler(HttpStatusCode.OK, settingGroup);
+        
+        var httpSettingsRepository = GetHttpSettingsRepository();
+        
+        var result = await httpSettingsRepository.ChangeTargetSettingVersion(request);
+        
+        Assert.Equal(settingGroup.Id, result.Id);
+    }
+    
+    [Theory]
+    [AutoTestData]
+    public async Task DeleteSettingGroup_Succeeds(string userId, string password, string settingGroupId)
+    {
+        var settingGroup = _fixture.Create<SettingGroup>();
+        
+        SetupHttpMessageHandler(HttpStatusCode.OK, settingGroup);
+        
+        var httpSettingsRepository = GetHttpSettingsRepository();
+        
+        var result = await httpSettingsRepository.DeleteSettingGroup(userId, password, settingGroupId);
+        
+        Assert.Equal(settingGroup.Id, result.Id);
+    }
+
+    [Theory]
+    [AutoTestData]
+    public async Task UpdatePermission_Succeeds(UpdatePermissionRequest request)
+    {
+        var permission = _fixture.Create<Permission>();
+        
+        SetupHttpMessageHandler(HttpStatusCode.OK, permission);
+        
+        var httpSettingsRepository = GetHttpSettingsRepository();
+        
+        var result = await httpSettingsRepository.UpdatePermission(request);
+
+        Assert.Equal(permission.SettingGroupId, result.SettingGroupId);
+        Assert.Equal(permission.UserId, result.UserId);
+    }
+
+    [Theory]
+    [AutoTestData]
+    public async Task PermissionRequestResponse_Succeeds(PermissionRequestResponse request)
+    {
+        var permission = _fixture.Create<Permission>();
+        
+        SetupHttpMessageHandler(HttpStatusCode.OK, permission);
+        
+        var httpSettingsRepository = GetHttpSettingsRepository();
+        
+        var result = await httpSettingsRepository.PermissionRequestResponse(request);
+        
+        Assert.Equal(permission.SettingGroupId, result.SettingGroupId);
+    }
+
+    private HttpSettingsRepository GetHttpSettingsRepository() => new (_httpClient, _config);
     
     private HttpResponseMessage GetHttpResponseMessage(HttpStatusCode statusCode, object content)
     {
@@ -31,5 +175,17 @@ public class HttpSettingsRepositoryTests
         {
             Content = new StringContent(JsonSerializer.Serialize(content))
         };
+    }
+    
+    private void SetupHttpMessageHandler(HttpStatusCode statusCode, object content)
+    {
+        _mockHttpMessageHandler
+            .Protected()
+            .Setup<Task<HttpResponseMessage>>(
+                "SendAsync",
+                ItExpr.IsAny<HttpRequestMessage>(),
+                ItExpr.IsAny<CancellationToken>()
+            )
+            .ReturnsAsync(GetHttpResponseMessage(statusCode, content));
     }
 }
--- AppSettingsManagerBff/AppSettingsManagerBff.Infrastructure.Tests/HttpUserRepositoryTests.cs
+++ AppSettingsManagerBff/AppSettingsManagerBff.Infrastructure.Tests/HttpUserRepositoryTests.cs
@@ -10,7 +10,7 @@ namespace AppSettingsManagerBff.Infrastructure.Tests;
 public class HttpUserRepositoryTests
 {
     private readonly Mock<HttpMessageHandler> _mockHttpMessageHandler = new();
-    
+
     private readonly AppSettingsManagerApiConfig _config = new AppSettingsManagerApiConfig
     {
         BaseAddress = "http://localhost:5000/"
@@ -21,38 +21,91 @@ public class HttpUserRepositoryTests
     public HttpUserRepositoryTests()
     {
         _httpClient = new HttpClient(_mockHttpMessageHandler.Object);
-        
+
         _httpClient.BaseAddress = new Uri(_config.BaseAddress + "users/");
     }
-    
+
     [Theory]
     [AutoTestData]
-    public async Task AuthenticateUser_ReturnsTrue_WhenUserIsAuthenticated(string userId, string password)
+    public async Task AuthenticateUser_ReturnsTrue_WhenUserIsAuthenticated(
+        string userId,
+        string password
+    )
     {
         SetupHttpMessageHandler(HttpStatusCode.OK, true);
-        
+
         var httpUserRepository = GetHttpUserRepository();
-        
+
         var result = await httpUserRepository.AuthenticateUser(userId, password);
-        
+
         Assert.True(result);
     }
-    
+
     [Theory]
     [AutoTestData]
-    public async Task AuthenticateUser_ReturnsFalse_WhenUserIsNotAuthenticated(string userId, string password)
+    public async Task AuthenticateUser_ReturnsFalse_WhenUserIsNotAuthenticated(
+        string userId,
+        string password
+    )
     {
         SetupHttpMessageHandler(HttpStatusCode.NotFound, false);
-        
+
         var httpUserRepository = GetHttpUserRepository();
-        
+
         var result = await httpUserRepository.AuthenticateUser(userId, password);
-        
+
         Assert.False(result);
     }
 
-    private HttpUserRepository GetHttpUserRepository() => new HttpUserRepository(_httpClient, _config);
+    [Theory]
+    [AutoTestData]
+    public async Task CreateUser_ReturnsUser_WhenUserIsCreated(CreateUserRequest request)
+    {
+        var user = new User { UserId = request.UserId, Password = request.Password };
+
+        SetupHttpMessageHandler(HttpStatusCode.OK, user);
+
+        var httpUserRepository = GetHttpUserRepository();
+
+        var result = await httpUserRepository.CreateUser(request);
+
+        Assert.Equal(user.UserId, result.UserId);
+        Assert.Equal(user.Password, result.Password);
+    }
     
+    [Theory]
+    [AutoTestData]
+    public async Task UpdateUserPassword_ReturnsUser_WhenUserIsUpdated(UpdateUserPasswordRequest request)
+    {
+        var user = new User { UserId = request.UserId, Password = request.NewPassword };
+
+        SetupHttpMessageHandler(HttpStatusCode.OK, user);
+
+        var httpUserRepository = GetHttpUserRepository();
+
+        var result = await httpUserRepository.UpdateUserPassword(request);
+
+        Assert.Equal(user.Password, result.Password);
+    }
+    
+    [Theory]
+    [AutoTestData]
+    public async Task DeleteUser_ReturnsUser_WhenUserIsDeleted(string userId, string password)
+    {
+        var user = new User { UserId = userId, Password = password };
+
+        SetupHttpMessageHandler(HttpStatusCode.OK, user);
+
+        var httpUserRepository = GetHttpUserRepository();
+
+        var result = await httpUserRepository.DeleteUser(userId, password);
+
+        Assert.Equal(user.UserId, result.UserId);
+        Assert.Equal(user.Password, result.Password);
+    }
+
+    private HttpUserRepository GetHttpUserRepository() => new(_httpClient, _config);
+
     private HttpResponseMessage GetHttpResponseMessage(HttpStatusCode statusCode, object content)
     {
         return new HttpResponseMessage(statusCode)
@@ -60,10 +113,11 @@ public class HttpUserRepositoryTests
             Content = new StringContent(JsonSerializer.Serialize(content))
         };
     }
-    
+
     private void SetupHttpMessageHandler(HttpStatusCode statusCode, object content)
     {
-        _mockHttpMessageHandler.Protected()
+        _mockHttpMessageHandler
+            .Protected()
             .Setup<Task<HttpResponseMessage>>(
                 "SendAsync",
                 ItExpr.IsAny<HttpRequestMessage>(),
